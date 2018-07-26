e8e49deab41fdbb5f89e9e3c920d63406f99803a
Stephen Chow <stephenchow2k@gmail.com>
2018-07-26 13:45:17 -0400

    Edited row information

--- client/components/ProductRow.js
+++ client/components/ProductRow.js
@@ -6,15 +6,16 @@ const ProductRow = props => {
       <img src={props.product.inventory.product.image} />
       <div>
         <h3>{props.product.inventory.product.name}</h3>
+        <h3>Size: {props.product.inventory.size}</h3>
         <h4>{props.product.inventory.product.description}</h4>
       </div>
-      <h4>{props.product.inventory.product.price}</h4>
+      <h4>${props.product.inventory.product.price}</h4>
       <form>
         <div>
           <button type="submit">^</button>
           <button type="submit">v</button>
         </div>
-        <selector>
+        <select value={props.product.quantity}>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => {
             return (
               <option value={num} key={num}>
@@ -22,7 +23,8 @@ const ProductRow = props => {
               </option>
             )
           })}
-        </selector>
+        </select>
+        <button type="submit">Update</button>
       </form>
     </React.Fragment>
   )
---
+++ client/store/cart.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const testInfo = [
+  {
+    userId: 1,
+    inventoryId: 1,
+    quantity: 3,
+    inventory: {
+      id: 1,
+      size: 'S',
+      quantity: 5,
+      productId: 1,
+      product: {
+        id: 1,
+        name: 'SquirrelSweater',
+        price: 20,
+        image: '/img/product/default-sweater-square.png',
+        description: 'testDescription'
+      }
+    }
+  },
+  {
+    userId: 1,
+    inventoryId: 2,
+    quantity: 2,
+    inventory: {
+      id: 2,
+      size: 'M',
+      quantity: 6,
+      productId: 1,
+      product: {
+        id: 1,
+        name: 'SquirrelSweater',
+        price: 20,
+        image: '/img/product/default-sweater-square.png',
+        description: 'testDescription'
+      }
+    }
+  }
+]
+const initialState = testInfo
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
