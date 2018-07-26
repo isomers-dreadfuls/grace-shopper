627ac19bd30c9115a703c34a55d31399f5a19435
Stephen Chow <36671331+iamstevechow@users.noreply.github.com>
2018-07-26 14:47:13 -0400

    Update cart.spec.js

--- server/db/models/cart.spec.js
+++ server/db/models/cart.spec.js
@@ -18,17 +18,17 @@ describe('Cart model', () => {
 //       }
 //     });
 
-    it('requires quantity', async () => {
-      const cart = Cart.build();
+//     it('requires quantity', async () => {
+//       const cart = Cart.build();
 
-      try {
-        await cart.validate()
-        throw Error('validation was successful but should have failed without `quantity`');
-      }
-      catch (err) {
-        expect(err.message).to.contain('quantity cannot be null');
-      }
-    });
+//       try {
+//         await cart.validate()
+//         throw Error('validation was successful but should have failed without `quantity`');
+//       }
+//       catch (err) {
+//         expect(err.message).to.contain('quantity cannot be null');
+//       }
+//     });
 
 //     it('requires inventory', async () => {
 //       const cart = Cart.build();
