851b9548cbe34c359f1ea930421d38ecbd4f3941
Stephen Chow <stephenchow2k@gmail.com>
2018-07-28 11:34:32 -0400

    Starting work on checkout

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import {Elements} from 'react-stripe-elements'
+import {StripeProvider, Elements} from 'react-stripe-elements'
 import InjectedCheckoutForm from './CheckoutForm'
 
 class Checkout extends React.Component {
+  constructor() {
+    super()
+    this.state = {stripe: null}
+  }
+  componentDidMount() {
+    if (window.Stripe) {
+      this.setState({stripe: window.Stripe('pk_test_PdTJvVNpt0qPtWbaasT8BAYN')})
+    } else {
+      document.querySelector('#stripe-js').addEventListener('load', () => {
+        // Create Stripe instance once Stripe.js loads
+        this.setState({
+          stripe: window.Stripe('pk_test_PdTJvVNpt0qPtWbaasT8BAYN')
+        })
+      })
+    }
+  }
   render() {
     return (
-      <Elements>
-        <InjectedCheckoutForm />
-      </Elements>
+      <StripeProvider stripe={this.state.stripe}>
+        <Elements>
+          <InjectedCheckoutForm />
+        </Elements>
+      </StripeProvider>
     )
   }
 }
