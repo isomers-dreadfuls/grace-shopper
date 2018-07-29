2137e674f11e776eb7db20896e8a96a07ef98f7a
Stephen Chow <stephenchow2k@gmail.com>
2018-07-29 00:02:23 -0400

    Created api spec files

---
+++ server/api/carts.spec.js
@@ -0,0 +1,13 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const User = db.model('user')
+
+describe('User routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+}) // end describe('User routes')
